6406203496ac5f09e1a2c468ac2d3fe45e59661c
Gary Russell <rugary@amazon.com>
2020-05-06 22:59:03 -0700

    monononono

--- rea/src/App.js
+++ rea/src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
- dasdf       MONO Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Monorepo react app.  Root is /rea
+	  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
