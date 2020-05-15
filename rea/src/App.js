ce3ac1ae4628369340a27199c9965f2f2ae290ed
Gary Russell <rugary@amazon.com>
2020-05-15 11:21:20 -0700

    baa

--- rea/src/App.js
+++ rea/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Monorepo react app.  Root is /rea. Updatesoxa this suckasdfasdfassdfasdfasdfa fuckso.
+        Monorepo react app.  Root is /rea. Updatesoxa this suckasdfasdfassdfasdfasdfa fucxxxxkso.
 	  </p>
         <a
           className="App-link"
