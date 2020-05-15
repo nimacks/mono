1e50eb6c127a9b0ea5b2b39e11b79b1d982b80a0
Gary Russell <rugary@amazon.com>
2020-05-15 00:20:30 -0700

    please don't build

--- rea/src/App.js
+++ rea/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Monorepo react app.  Root is /rea. Update.
+        Monorepo react app.  Root is /rea. Updatessss.
 	  </p>
         <a
           className="App-link"
