c86883c3ed3b360815c9d14d36f9e53be69aa3f6
Sam <4107271+nimacks@users.noreply.github.com>
2021-03-02 21:16:54 -0800

    Update App.js

--- rea/src/App.js
+++ rea/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Monorepo react app.  Root is /rea. 
+        Monorepo react app. a  Root is /rea. 
         {process.env.REACT_APP_PHRASE}
 	  </p>
         <a
