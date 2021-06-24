d3b15fd024735c65534be9735cae9917f09d48c3
Sam <4107271+nimacks@users.noreply.github.com>
2021-06-23 17:22:30 -0700

    Update App.js

--- other/src/App.js
+++ other/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is monorepo react app.  Root /other
+         This is the monorepo react app.  Root /other
         {process.env.REACT_APP_PHRASE} 
     </p>
         <a
